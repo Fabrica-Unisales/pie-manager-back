5421e65bbc8fb19a08a6bd8792045b2b14c75b9c
James Alves <james@jamesalves.com.br>
2025-05-20 21:49:17 -0300

    alguma correcoes e add database no  gitignore

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,5 @@ import { Sequelize } from 'sequelize-typescript';
 
 @Injectable()
 export class AppService {
-  constructor(private sequelize: Sequelize) {
-    sequelize.sync();
-  }
+  constructor(private sequelize: Sequelize) {}
 }
