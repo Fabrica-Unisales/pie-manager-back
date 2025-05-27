ea75c52ef4bb496d004682a1d0e02dfea8a42d35
James Alves <james@jamesalves.com.br>
2025-05-27 19:50:07 -0300

    fix: service reference on module

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -6,10 +6,11 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from 'src/users/entities/user.entity';
 import { Group } from 'src/groups/entities/group.entity';
 import { AlocacaoController } from './alocacao-projects.controller';
+import { AlocacaoService } from './alocacao-projects.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Project, User, Group])],
   controllers: [ProjectsController, AlocacaoController],
-  providers: [ProjectsService, AlocacaoController],
+  providers: [ProjectsService, AlocacaoService],
 })
 export class ProjectsModule { }
