bd8e41265dce5405fa13f1c28b64602a0b5067d3
James Alves <james@jamesalves.com.br>
2025-06-03 20:15:46 -0300

    add userTurmo on group module

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -7,12 +7,13 @@ import { UserGroup } from './entities/user-group.entity';
 import { Turma } from '../turma/entities/turma.entity';
 import { User } from '../users/entities/user.entity';
 import { TurmaModule } from 'src/turma/turma.module';
+import { UserTurma } from 'src/turma/entities/user-turma.entity';
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([Group, UserGroup, Turma, User]),
-    TurmaModule
-],
+    SequelizeModule.forFeature([Group, UserGroup, Turma, User, UserTurma]),
+    TurmaModule,
+  ],
   controllers: [GroupsController],
   providers: [GroupsService],
   exports: [SequelizeModule],
